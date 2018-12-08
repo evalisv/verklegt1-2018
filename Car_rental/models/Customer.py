39ac4ae0f6ff8650e1bb4c62d8e8733f55863f62
Eva Lísa <evalisv@gmail.com>
2018-12-08 15:51:32 +0000

    Customer in models updated

--- Car_rental/models/Customer.py
+++ Car_rental/models/Customer.py
@@ -11,5 +11,29 @@ class Customer:
         self.__cc_number = cc_number
 
     def __str__(self):
-        return "{},{},{},{},{},{},{},{},".format(self.__id_number, self.__name, self.__age, self.__country, self.__email, self.__phone,\
-         self.__dl_number, self.__cc_number)
+        return "{},{},{},{},{},{},{},{},".format(
+            self.__id_number, self.__name, self.__age, self.__country, self.__email, self.__phone, self.__dl_number, self.__cc_number)
+        
+    def get_id_number(self):
+        return self.__id_number
+
+    def get_name(self):
+        return self.__name
+    
+    def get_age(self):
+        return self.__age
+    
+    def get_country(self):
+        return self.__country
+
+    def get_email(self):
+        return self.__email
+    
+    def get_phone(self):
+        return self.__phone
+    
+    def get_dl_number(self):
+        return self.__dl_number
+
+    def get_cc_number(self):
+        return self.__cc_number
