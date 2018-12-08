2a99e45f2f6f122a664236f8e26ceb9f6872f6a7
Eva Lísa <evalisv@gmail.com>
2018-12-08 15:42:31 +0000

    Customer in models updated

---
+++ Car_rental/models/Customer.py
@@ -0,0 +1,15 @@
+class Customer:
+
+    def __init__(self, id_number, name, age, country, email, phone, dl_number, cc_number):
+        self.__id_number = id_number
+        self.__name = name
+        self.__age = age
+        self.__country = country
+        self.__email = email
+        self.__phone = phone
+        self.__dl_number = dl_number
+        self.__cc_number = cc_number
+
+    def __str__(self):
+        return "{},{},{},{},{},{},{},{},".format(self.__id_number, self.__name, self.__age, self.__country, self.__email, self.__phone,\
+         self.__dl_number, self.__cc_number)
