ff7a2d448b3f20eeacb7c9d7ab340296333e30dc
dilja <dilja16@ru.is>
2018-12-09 17:06:46 +0000

    Bjó til alveg eins gaura fyrir CustomerService og OrderService eins og var til fyrir CarService - Á eftir að vinna betur með en er að prófa að committa

---
+++ Car_rental/Services/CustomerService.py
@@ -0,0 +1,20 @@
+from repositories.CustomerRepo import CustomerRepository
+
+class CustomerService():
+    def __init__(self):
+        self.__customer_repo = CustomerRepository()
+
+    def add_customer(self, customer):
+        if self.is_valid_customer(customer):
+            self.__customer_repo.add_car(customer)
+    
+    def is_valid_customer(self, customer):
+        #here should be some code to 
+        #validate the customer
+        return True
+
+    def get_customers(self):
+        return self.__customer_repo.get_customer()
+
+    def get_customers_by_status(self, status):
+        pass
