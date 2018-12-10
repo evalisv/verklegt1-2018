e6897a9057ee73028884955eddb44e6db2657a77
Eva Lísa <evalisv@gmail.com>
2018-12-10 12:54:16 +0000

    Add a function to csv_print_functions

--- Car_rental/Eva_testing_field/csv_reader_test.py
+++ Car_rental/Eva_testing_field/csv_reader_test.py
@@ -3,16 +3,24 @@ import csv
 with open ("Eva_testing_field/customer.csv", "r") as csv_file:
     csv_reader = csv.DictReader(csv_file)
 
-    match_value = 1
+    match1_value = 1
+    match2_value = 1
     customer_id_filter = input("Customer ID: ")
+    age_filter = input("Age: ")
     for line in csv_reader:
         
-        if line["Customer ID"] == customer_id_filter:
-            print(line["Name"], "\t", line["Email"], "\t", line["Phone"])
-            match_value += 1
+        if line["Customer ID"] == customer_id_filter or line["Age"] == age_filter:
+            print(line["Name"], "\t", line["Age"], "\t", line["Phone"])
+            if line["Customer ID"] == customer_id_filter:
+                match1_value += 1
+            if line["Age"] == age_filter:
+                match2_value += 1
         
-    if match_value == 1:
+    if match1_value == 1 and customer_id_filter != "":
         print(customer_id_filter, "was not found")
+    
+    if match2_value == 1 and age_filter != "":
+        print("No customers are", age_filter, "years old.")
         
         
 
