55d7035771b644aa37aba404dad1f68b016b5fc5
Sigurður Sindri Helgason <siggish@gmail.com>
2018-12-13 18:11:24 +0000

    Update Order.py

--- Car_rental/models/Order.py
+++ Car_rental/models/Order.py
@@ -1,39 +1,39 @@
 class Order:
     
     def __init__(self, number = '', customer_id = '', lp_number = '', category = '', pickup_date = '', return_date = '', price = '', insurance = ''):
-        self.__number = number
-        self.__customer_id = customer_id
-        self.__lp_number = lp_number
-        self.__category = category
-        self.__pickup_date = pickup_date
-        self.__return_date = return_date
-        self.__price = price
-        self.__insurance = insurance
+        self.number = number
+        self.customer_id = customer_id
+        self.lp_number = lp_number
+        self.category = category
+        self.pickup_date = pickup_date
+        self.return_date = return_date
+        self.price = price
+        self.insurance = insurance
 
     def __str__(self):
         return "{},{},{},{},{},{},{},".format(
-            self.__number, self.__customer_id, self.__lp_number, self.__pickup_date, self.__return_date, self.__price, self.__insurance)
+            self.__number, self.customer_id, self.lp_number, self.pickup_date, self.return_date, self.price, self.insurance)
 
     def get_order_number(self):
-        return self.__number
+        return self.number
 
     def get_customer_id(self):
-        return self.__customer_id
+        return self.customer_id
     
     def get_lp_number(self):
-        return self.__lp_number
+        return self.lp_number
 
     def get_category(self):
-        return self.__category
+        return self.category
 
     def get_pickup_date(self):
-        return self.__pickup_date
+        return self.pickup_date
 
     def get_return_date(self):
-        return self.__return_date
+        return self.return_date
 
     def get_price(self):
-        return self.__price
+        return self.price
 
     def get_insurance(self):
-        return self.__insurance
+        return self.insurance
