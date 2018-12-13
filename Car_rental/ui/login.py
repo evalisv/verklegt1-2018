a09e60deeaec940c8fccd7b7d64545f336fcc2d4
Jökull <jokullvidar@gmail.com>
2018-12-13 10:29:49 +0000

    fixed login so it starts at first line again

--- Car_rental/ui/login.py
+++ Car_rental/ui/login.py
@@ -6,8 +6,8 @@ def login():
         database = csv.DictReader(csvfile)
         loggedin = False
         while loggedin is not True:
-            Username = input('Username: ')
-            Password = input('Password: ')
+            Username = input('Username: ').lower()
+            Password = input('Password: ').lower()
             for row in database:
                 Username_File = row['username']
                 Password_File = row['password']
@@ -18,13 +18,12 @@ def login():
                     loggedin = True
                     print('Succesfully logged in.')
                     return user_access
-                    # patientmenu()  # we will add this later on
                 if (Username_File == Username and
                     Password_File == Password and
                         user_access == 'admin'):
                     loggedin = True
                     print('Succesfully logged in as an admin.')
                     return user_access
-                    # artsmenu()     # we will add this later on
             if loggedin == False:
                 print('wrong username or password')
+            csvfile.seek(0,0)
